fc4d8b39c9983a2e73d93e2851d8052ea4fe30c2
Neylson Crepalde <neylsoncrepalde@gmail.com>
2020-11-16 06:36:25 -0300

    atualizacoes para execucao

--- prefect/pref01.py
+++ prefect/pref01.py
@@ -1,28 +1,36 @@
 from datetime import datetime, timedelta
 import prefect
-from prefect import Parameter, task, Flow
-from prefect.tasks.notifications.email_task import EmailTask
+from prefect import task, Flow
 from prefect.schedules import IntervalSchedule
 import pandas as pd
 
 retry_delay = timedelta(minutes=1)
+schedule = IntervalSchedule(interval=timedelta(minutes=2))
 
 @task
 def get_data():
-    return pd.read_csv('https://raw.githubusercontent.com/A3Data/hermione/master/hermione/file_text/train.csv')
+    df = pd.read_csv("https://raw.githubusercontent.com/A3Data/hermione/master/hermione/file_text/train.csv")
+    return df
 
 @task
-def calculate_mean_age(df):
+def calcula_media_idade(df):
     return df.Age.mean()
 
 @task
-def print_mean(m):
-    print(f"A média calculada foi {m}")
+def exibe_media_calculada(m):
+    logger = prefect.context.get("logger")
+    logger.info(f"A média de idade calculada foi {m}")
 
-with Flow("Titanic01") as flow:
-    data = get_data()
-    med = calculate_mean_age(data)
-    p = print_mean(med)
+@task
+def exibe_dataset(df):
+    logger = prefect.context.get("logger")
+    logger.info(df.head(3).to_json())
 
+with Flow("Titanic01", schedule=schedule) as flow:
+    df = get_data()
+    med = calcula_media_idade(df)
+    e = exibe_media_calculada(med)
+    ed = exibe_dataset(df)
 
 flow.register(project_name='IGTI', idempotency_key=flow.serialized_hash() )
+flow.run_agent(token="0dZVAbOVJZ6ZZawMdmocFQ")
